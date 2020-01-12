2bf5e33ebd14fc90075f91224b60f755fa231e31
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-12 11:20:48 -0800

    downloaded CORS package to handle CORS issue for requests coming from parcel server per express documentation.  Requests are now working properly

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     postTest: (inputValue) => {
-       return axios.post("/api", {
+       return axios.post("http://localhost:3000/api", {   //** Change this route back to a relative path when pushing project to heroku.  Only using absolute path during Dev process**//
         testValue: inputValue,
     })
 } 
