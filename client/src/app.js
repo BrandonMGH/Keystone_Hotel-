45aa28c9a580a5c6c2465991c656b7cbf8329c78
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-11 21:45:27 -0800

    set up an axios call on the front end to send a request to the backend, butfrom the backend to the back end

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,25 @@
-import React from "React"
+import React, { useState } from 'react';
+const axios = require('axios');
 
 const App = () => {
+    const [inputValue, setInputValue] = useState("")
+    let axiosCall = () => {
+        axios.post("http://localhost:3000/api", {
+            testValue: inputValue,
+        })
+        .then((response) =>{
+            console.log(response)
+        })
+        .catch((error) =>{
+            console.log(error)
+        })
+    }
     return (
         <div>
-            This is the App Component 
+            <label>Test Input</label>
+            <input id="title" onChange={event => setInputValue(event.target.value)}></input>
+            <button onClick={axiosCall}>Submit Button</button>
+            
         </div>
     )
 }
