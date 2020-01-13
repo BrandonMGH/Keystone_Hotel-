6dd0a6a7fff63b495602b15cb4bb566fccd799c3
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-12 22:57:32 -0800

    downloaded styled components and rect-router-dom.  Set up a Main.js folder/file that will serve as the main vertical landing page for the website

--- client/src/app.js
+++ client/src/app.js
@@ -1,25 +1,13 @@
-import React, { useState } from 'react';
-import API from './API/API.js';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import Main from './Main/Main.js'
 
 const App = () => {
-    const [inputValue, setInputValue] = useState("")
-    let axiosCall = () => {
-        API.postTest(inputValue)
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
     return (
-        <div>
-            <label>Test Input</label>
-            <input id="title" onChange={event => setInputValue(event.target.value)}></input>
-            <button onClick={axiosCall}>Submit Button</button>
-
-        </div>
+        <Router>
+            <Route path="/" exact component={Main} />
+        </Router>
     )
 }
 
