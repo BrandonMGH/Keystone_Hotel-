89d1a344c5170d6d0ba0053597f4a9dac2d7bcbf
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-14 11:18:29 -0800

    Working on pop up reservations modal

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 
 const StyleTest = styled.div`
-display: ${props => props.showState}
-
+display: ${props => props.showState};
+justify-content: center; 
+background: papayawhip;
+width: 50%
+z-index: 1000 
 `
 
 let Navbar = () => {
 
-const [reservationTab, setReservationTab] = useState(true)
+    const [reservationTab, setReservationTab] = useState(true)
 
-const reservationState = () => {
-    if(reservationTab === true){
-        setReservationTab(false)
-    } else{
-        setReservationTab(true)
+    const reservationState = () => {
+        if (reservationTab === true) {
+            setReservationTab(false)
+        } else {
+            setReservationTab(true)
+        }
     }
-}
 
     return (
         <div>
-            <StyleTest showState={reservationTab === true ? "Block" : "None"}>
-                <p>Reservation Box</p>
-            </StyleTest>
+            <div style={{margin: "auto"}}>
+            <StyleTest showState={reservationTab === true ? "grid" : "None"}>Reservation Box</StyleTest>
+            </div>
             <button onClick={reservationState}> Reserve a Room</button>
         </div>
     )
