cafcd02ef2165640de857ccce4b9077fe6ad375b
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-26 00:10:31 -0800

    worked on styling the mainSectionDivideTwo

--- client/src/Pages/Main/Main.js
+++ client/src/Pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Main.css'
 
-import ThailandBoatHoping from './Images/ThailandBoatHoping.jpg'
+import girlMountain from './Images/girlMountain.jpeg'
 
 
 
@@ -11,7 +11,7 @@ const Main = () => {
     return (
         <>
             <section id="mainTitleSection">
-                <section id="mainTitleSectionText">
+                <section className="sectionTextFormatting">
                     <h1>-KEYSTONE HOTEL-</h1>
                 </section>
             </section>
@@ -21,9 +21,13 @@ const Main = () => {
                     <p>Good. Use your aggressive feelings, boy! Let the hate flow through you. Obi-Wan has taught you well. I will not fight you, father. You are unwise to lower your defenses. Your thoughts betray you, father. I feel the good in you...the conflict. There is no conflict. You couldn't bring yourself to kill me before, and I don't believe you'll destroy me now. You underestimate the power of the dark side. If you will not fight, then you will meet your destiny. Good. Good.</p>
                 </section>
                 <section id="mainSubSectionDivideTwo">
-                    <img id="imgSubSectionDivideTwo" src={ThailandBoatHoping} />
+                    <img id="imgSubSectionDivideTwo" src={girlMountain} />
                 </section>
-
+            </section>
+            <section id="mainSectionDivideTwo">
+                <section className="sectionTextFormatting">
+                <h1>“A mind that is stretched by a new experience can never go back to its old dimensions.” - Oliver Wendell Holmes</h1>
+                </section>
             </section>
 
         </>
