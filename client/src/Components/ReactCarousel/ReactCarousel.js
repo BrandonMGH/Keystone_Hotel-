2603db0e7358d5e4a21fa1c1d9f855ec3cfd2acb
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-27 11:01:12 -0800

    brought back old react-responsive-carousel as the new carousel package was not working as intended

--- client/src/Components/ReactCarousel/ReactCarousel.js
+++ client/src/Components/ReactCarousel/ReactCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import './ReactCarousel.css'
 
 import HotelBarOne from '../../Images/HotelBarOne.jpeg'
 import HotelBarTwo from '../../Images/HotelBarTwo.jpg'
@@ -10,20 +11,36 @@ const ReactCarousel = () => {
     return (
         <Carousel autoPlay showThumbs={false}>
             <div>
-                <h1 className="legend" style={{fontSize: "50px", position: "relative", top: "300px"}}>Test</h1>
-                <img src={HotelBarOne} />
+                <section id="carouselTextOne">
+                    <h1 id="carouselTitleTextOne">ROOMS</h1>
+                    <p>If you would like to find out more information about the rooms, please click the link below</p>
+                    <a>Read More</a>
+                </section>
+                <img id="carouselImgOne" src={HotelBarOne} />
             </div>
             <div>
-                <h1 className="legend">Test</h1>
-                <img src={HotelBarTwo} />
+                <section id="carouselTextTwo">
+                    <h1 id="carouselTitleTextTwo">DINING</h1>
+                    <p>If you would like to find out more information about the rooms, please click the link below</p>
+                    <a>Read More</a>
+                </section>
+                <img id="carouselImgTwo" src={HotelBarTwo} />
             </div>
             <div>
-                <h1 className="legend">Test</h1>
-                <img src={HotelBarThree} />
+                <section id="carouselTextThree">
+                    <h1 id="carouselTitleTextThree">SPA</h1>
+                    <p>If you would like to find out more information about the rooms, please click the link below</p>
+                    <a>Read More</a>
+                </section>
+                <img id="carouselImgThree" src={HotelBarThree} />
             </div>
             <div>
-                <h1 className="legend">Test</h1>
-                <img src={HotelBarThree} />
+                <section id="carouselTextFour">
+                    <h1 id="carouselTitleTextFour">AREA ATTRACTION</h1>
+                    <p>If you would like to find out more information about the rooms, please click the link below</p>
+                    <a>Read More</a>
+                </section>
+                <img id="carouselImgFour" src={HotelBarThree} />
             </div>
         </Carousel>
     )
