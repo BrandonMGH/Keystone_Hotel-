13c1ae8f05541eee0fa75f1487b70df3b00a7661
BrandonMGH <BrandonMGH@Yahoo.com>
2020-01-31 11:11:34 -0800

    finished some basic styling for RoomViewTypes for display in the Rooms component

--- client/src/Components/RoomViewTypes/LakeViewRoom.js
+++ client/src/Components/RoomViewTypes/LakeViewRoom.js
@@ -1,13 +1,37 @@
 import React from "react"
 import RoomViewData from "./RoomViewData/RoomViewData.js"
+import "./RoomViews.css"
 
 const LakeViewRoom = () => {
     return (
-        <div>
-            {RoomViewData.LakeView.map(properties=> (
-                <h1>{properties.RoomTitle}</h1>
+        <section>
+            {RoomViewData.LakeView.map(properties => (
+                <section className="roomViewContainer" key={properties.id}>
+                    <img className="roomViewRoomImage" src={properties.RoomImage} />
+                    <section className="roomViewTextContainer">
+                        <section className="roomViewTextTitleAndDescription">
+                            <p className="roomViewTextTitle">{properties.RoomTitle}</p>
+                            <p>{properties.RoomDescription}</p>
+                        </section>
+                        <section className="roomViewTextAmenities">
+                            <h4>Room Amenities</h4>
+                            <p>________________</p>
+                            <p>{properties.RoomInfoAndAmenities.Size}</p>
+                            <p>{properties.RoomInfoAndAmenities.BedType}</p>
+                            <p>{properties.RoomInfoAndAmenities.Internet}</p>
+                            <p>{properties.RoomInfoAndAmenities.Television}</p>
+                            <p>{properties.RoomInfoAndAmenities.Bathroom}</p>
+                            <p>{properties.RoomInfoAndAmenities.CoffeeMaker}</p>
+                            <p>{properties.RoomInfoAndAmenities.Fridge}</p>
+                            <p>{properties.RoomInfoAndAmenities.Robes}</p>
+                        </section>
+                        <section className="roomViewViewImageContainer">
+                            <img className="roomViewViewImage"src={properties.ViewImage} />
+                        </section>
+                    </section>
+                </section>
             ))}
-        </div>
+        </section>
     )
 }
 
---
+++ client/src/Components/RoomViewTypes/MountainViewRoom.js
@@ -0,0 +1,38 @@
+import React from "react"
+import RoomViewData from "./RoomViewData/RoomViewData.js"
+import "./RoomViews.css"
+
+const MountainViewRoom = () => {
+    return (
+        <section>
+            {RoomViewData.MountainView.map(properties => (
+                <section className="roomViewContainer" key={properties.id}>
+                    <img className="roomViewRoomImage" src={properties.RoomImage} />
+                    <section className="roomViewTextContainer">
+                        <section className="roomViewTextTitleAndDescription">
+                            <p className="roomViewTextTitle">{properties.RoomTitle}</p>
+                            <p>{properties.RoomDescription}</p>
+                        </section>
+                        <section className="roomViewTextAmenities">
+                            <h4>Room Amenities</h4>
+                            <p>________________</p>
+                            <p>{properties.RoomInfoAndAmenities.Size}</p>
+                            <p>{properties.RoomInfoAndAmenities.BedType}</p>
+                            <p>{properties.RoomInfoAndAmenities.Internet}</p>
+                            <p>{properties.RoomInfoAndAmenities.Television}</p>
+                            <p>{properties.RoomInfoAndAmenities.Bathroom}</p>
+                            <p>{properties.RoomInfoAndAmenities.CoffeeMaker}</p>
+                            <p>{properties.RoomInfoAndAmenities.Fridge}</p>
+                            <p>{properties.RoomInfoAndAmenities.Robes}</p>
+                        </section>
+                        <section className="roomViewViewImageContainer">
+                            <img className="roomViewViewImage"src={properties.ViewImage} />
+                        </section>
+                    </section>
+                </section>
+            ))}
+        </section>
+    )
+}
+
+export default MountainViewRoom
