922e1215d2c5c1bd174d2acac997f638b716ad4c
BrandonMGH <BrandonMGH@Yahoo.com>
2020-02-04 13:01:33 -0800

    update

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 
 export default {
     getRoomInfo: () => {
-        return axios.get("/reservations")
+        return axios.get("https://keystone-hotel.herokuapp.com/api/reservations")
     },
     upNewsletterInfo: (inputValue) => {
-        return axios.post("/api/newsletter", {
+        return axios.post("https://keystone-hotel.herokuapp.com/api/newsletter", {
             newsletterObject: inputValue
         })
     },
     reservationConfirmation: (roomInfoObject, reservationInfoObject) => {
-        return axios.post("/reservations/confirmation", {
+        return axios.post("https://keystone-hotel.herokuapp.com/api/reservations/confirmation", {
             roomConfirmObject: roomInfoObject,
             resConfirmObject: reservationInfoObject, 
         })
     },
     reservationInfo: (inputValue) => {
-        return axios.put("/reservations", {   //** Change this route back to a relative path when pushing project to heroku.  Only using absolute path during Dev process**//
+        return axios.put("https://keystone-hotel.herokuapp.com/api/reservations", {   //** Change this route back to a relative path when pushing project to heroku.  Only using absolute path during Dev process**//
             resObject: inputValue,
         })
     },
