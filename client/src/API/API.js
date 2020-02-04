dafce78d88360f5268a87845553857f6e9487465
BrandonMGH <BrandonMGH@Yahoo.com>
2020-02-04 12:55:23 -0800

    added environmental variables to Heroku and set the API routes relative paths instead of absolute paths

--- client/src/API/API.js
+++ client/src/API/API.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 
 export default {
     getRoomInfo: () => {
-        return axios.get("http://localhost:3001/api/reservations")
+        return axios.get("/reservations")
     },
     upNewsletterInfo: (inputValue) => {
-        return axios.post("http://localhost:3001/api/newsletter", {
+        return axios.post("/api/newsletter", {
             newsletterObject: inputValue
         })
     },
     reservationConfirmation: (roomInfoObject, reservationInfoObject) => {
-        return axios.post("http://localhost:3001/api/reservations/confirmation", {
+        return axios.post("/reservations/confirmation", {
             roomConfirmObject: roomInfoObject,
             resConfirmObject: reservationInfoObject, 
         })
     },
     reservationInfo: (inputValue) => {
-        return axios.put("http://localhost:3001/api/reservations", {   //** Change this route back to a relative path when pushing project to heroku.  Only using absolute path during Dev process**//
+        return axios.put("/reservations", {   //** Change this route back to a relative path when pushing project to heroku.  Only using absolute path during Dev process**//
             resObject: inputValue,
         })
     },
