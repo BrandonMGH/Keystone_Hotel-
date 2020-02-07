927307a831ee4b52da16ed44de17f7cbefb16717
BrandonMGH <BrandonMGH@Yahoo.com>
2020-02-06 19:32:52 -0800

    working on the Restaurants.js component inside of the Dining.js page component

---
+++ client/src/Components/RestaurantTypes/RestaurantTypeData/RestaurantTypeData.js
@@ -0,0 +1,65 @@
+
+const RestaurantTypeData = {
+    RestaurantOne: {
+        restaurantImage: "",
+        restaurantTitle: "The Snowy Tavern",
+        restaurantDescriptionOne: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+        restaurantDescriptionTwo: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+        restaurantMenuInfo: {
+            menuItemOneTitle:"Menu Item One",
+            menuItemOneDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemOnePrice: "$10",
+            menuItemTwoTitle:"Menu Item One",
+            menuItemTwoDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemTwoPrice: "$10",
+            menuItemThreeTitle:"Menu Item One",
+            menuItemThreeDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemThreePrice: "$10",
+            menuItemFourTitle:"Menu Item One",
+            menuItemFourDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemFourPrice: "$10",
+        }
+    },
+    RestaurantTwo: {
+        restaurantImage: "",
+        restaurantTitle: "The Snowy Tavern",
+        restaurantDescriptionOne: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+        restaurantDescriptionTwo: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+        restaurantMenuInfo: {
+            menuItemOneTitle:"Menu Item One",
+            menuItemOneDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemOnePrice: "$10",
+            menuItemTwoTitle:"Menu Item One",
+            menuItemTwoDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemTwoPrice: "$10",
+            menuItemThreeTitle:"Menu Item One",
+            menuItemThreeDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemThreePrice: "$10",
+            menuItemFourTitle:"Menu Item One",
+            menuItemFourDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemFourPrice: "$10",
+        }
+    },
+    RestaurantTwo: {
+        restaurantImage: "",
+        restaurantTitle: "The Snowy Tavern",
+        restaurantDescriptionOne: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+        restaurantDescriptionTwo: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+        restaurantMenuInfo: {
+            menuItemOneTitle:"Menu Item One",
+            menuItemOneDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemOnePrice: "$10",
+            menuItemTwoTitle:"Menu Item One",
+            menuItemTwoDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemTwoPrice: "$10",
+            menuItemThreeTitle:"Menu Item One",
+            menuItemThreeDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemThreePrice: "$10",
+            menuItemFourTitle:"Menu Item One",
+            menuItemFourDescription: "Leave that to me! Damn fool. I knew that you were going to say that! Who's the more foolish...the fool or the fool who follows him?",
+            menuItemFourPrice: "$10",
+        }
+    },
+}
+
+export default RestaurantTypeData
--- client/src/Pages/Dining/Dining.js
+++ client/src/Pages/Dining/Dining.js
@@ -3,6 +3,10 @@ import './Dining.css'
 
 import GrapeIcon from '../../Images/GrapeIcon.png'
 
+/* COMPONENTS */ 
+import Restaurants from '../../Components/RestaurantTypes/Restaurants.js'
+import Restaurant from '../../Components/RestaurantTypes/Restaurants.js'
+
 const Dining = () => {
     return (
         <>
@@ -24,6 +28,9 @@ const Dining = () => {
                 <section id="diningSectionTwoDescription">
                     <p>Luke, what's wrong? Leia... do you remember your mother? Your real mother? Just a little bit. She died when I was very young. What do you remember? Just...images, really. Feelings. Tell me. She was very beautiful. Kind, but...sad. Why are you asking me all this? I have no memory of my mother. I never knew her. Luke, tell me. What's troubling you? Vader is here...now, on this moon. How do you know? I felt his presence. He's come for me. He can feel when I'm near. That's why I have to go. As long as I stay, I'm endangering the group and our mission here. I have to face him. Why? He's my father. Your father? There's more. It won't be easy for you to hear it, but you must.</p>
                 </section>
+            </section>
+            <section>
+                <Restaurant />
             </section>
         </>
     )
