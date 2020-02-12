12b5105612c9bbb744e248eba7cc75da51ff2e62
BrandonMGH <BrandonMGH@Yahoo.com>
2020-02-11 22:42:44 -0800

    styling Spa.js

--- client/src/Pages/Spa/Spa.js
+++ client/src/Pages/Spa/Spa.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Spa.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpa, faDumbbell, faPray, faHotTub } from '@fortawesome/free-solid-svg-icons'
+
 import SpaRocks from '../../Images/SpaRocks.png'
+import facialheader2 from '../../Images/facialheader2.jpg'
+import spaFacial from '../../Images/spaFacial.webp'
+
 const Spa = () => {
     return (
         <section>
@@ -20,6 +26,36 @@ const Spa = () => {
                 </section>
                 <section id="spaSectionTwoDescriptionContainer">
                     <p>What a piece of junk. She'll make point five beyond the speed of light. She may not look like much, but she's got it where it counts, kid. I've added some special modifications myself. We're a little rushed, so if you'll hurry aboard we'll get out of here. Hello, sir. Which way? All right, men. Load your weapons! Stop that ship! Blast 'em! Chewie, get us out of here! Oh, my. I'd forgotten how much I hate space travel.</p>
+                </section>
+            </section>
+            <section id="spaSectionThree">
+                <section id="spaSectionThreeText">
+                    <section id="spaSectionThreeTitle">
+                        <h1>SPA & WELLNESS AT A GLANCE</h1>
+                    </section>
+                    <section id="spaSectionThreeAmenitites">
+                        <section style={{marginBottom: "50px"}}>
+                            <p>SPA</p>
+                            <FontAwesomeIcon size='4x' icon={faSpa} />
+                        </section>
+                        <section style={{marginBottom: "50px"}}>
+                            <p>FITNESS CENTER</p>
+                            <FontAwesomeIcon size='4x' icon={faDumbbell} />
+                        </section>
+                        <section style={{marginBottom: "50px"}}>
+                            <p>OUTDOOR YOGA</p>
+                            <FontAwesomeIcon size='4x'
+                            icon={faPray} />
+                        </section>
+                        <section style={{marginBottom: "50px"}}>
+                            <p>STEAM ROOM</p>
+                            <FontAwesomeIcon size='4x'
+                            icon={faHotTub} />
+                        </section>
+                    </section>
+                </section>
+                <section id="spaSectionThreeImage">
+                    <img style={{width: "90%", height: "90%"}}src={spaFacial} />
                 </section>
             </section>
         </section>
